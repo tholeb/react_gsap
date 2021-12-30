288490ff13693b03e17e3c5d8e51683da9bf9a2d
tholeb <thomasleb.49@gmail.com>
2021-12-30 01:01:12 +0100

    Add: ts config and modules

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/// <reference types="react-scripts" />
+
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.webm' {
+    const src: string;
+    export default src;
+}
