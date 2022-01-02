b368ac17ecac18c5d10dc8bfc0e364390e3654dc
tholeb <thomasleb.49@gmail.com>
2022-01-02 12:40:26 +0100

    Change: video as background

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -7,19 +7,39 @@ const VideoComponent = (props: { progress: number }) => {
 
 	const ref = useRef(null);
 
+
 	useEffect(() => {
-		const vid = ref.current!;
+		// const vid = ref.current!;
 
-		const duration = vid.duration || 1;
+		// const duration = vid.duration;
+		// const currentTime = progress * duration;
 
-		if (vid) {
-			vid.currentTime = progress * duration;
-		}
-		console.log(progress, duration, vid.currentTime, progress * vid.duration);
+		// if (currentTime) {
+		// 	vid.currentTime = currentTime;
+		// }
+
+		// if (progress === 0 || progress === 1) {
+		// 	vid.pause();
+		// }
+		// else {
+		// 	vid.play();
+		// }
+
+		// if (progress <= 0.1) {
+		// 	vid.style.opacity = progress;
+		// }
+		// else if (progress >= 0.9) {
+		// 	vid.style.opacity = 1 - progress;
+		// }
+		// else {
+		// 	vid.style.opacity = 1;
+		// }
+		// console.log(progress, duration, vid.currentTime, progress * vid.duration);
 	}, [progress]);
 
 	return (
-		<video ref={ref} style={{ width: '100vw' }}>
+	    // style={{ transition: 'opacity 1s ease-in-out', opacity: 0 }}
+		<video ref={ref} autoPlay muted loop className="object-cover w-screen h-screen" style={{ transition: 'opacity 1s ease-in-out', opacity: 1 }}>
 			<source src={tigerwebm} type="video/webm" />
 
 			<source src={tigermp4} type="video/mp4" />
